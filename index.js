b07606633470f08fe68108a357ad62d99535d515
Bobo <bobo.kovacevic@gmail.com>
2022-05-09 23:08:00 +0200

    unique active wallets

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/blockdata/daily/30days', async (req, res) => {
-  const result = await axios.post(API_URL, {
-    query: 'query { dailyCounts (last:30, orderBy:ID_ASC) { nodes { id, nativeActiveUsers, evmActiveUsers} } }'
-  });
-  const data = result.data.data.dailyCounts.nodes;
+  const data = await getDailyData();
   let csv = 'Date, NativeActiveUsersCount, EVMActiveUsersCount\n';
   data.map(item => {
     csv += `${item.id}, ${item.nativeActiveUsers.length}, ${item.evmActiveUsers.length}` + '\n';
@@ -18,6 +15,30 @@ app.get('/blockdata/daily/30days', async (req, res) => {
   res.header('Content-Type', 'text/csv');
   res.attachment('daily.csv');
   return res.send(csv);
+});
+
+app.get('/blockdata/daily-unique/30days', async (req, res) => {
+  const data = await getDailyData();
+  const native = [];
+  const evm = []
+  data.map(item => {
+    item.nativeActiveUsers.map(x => {
+      if(!native.includes(x)) {
+        native.push(x);
+      }  
+    });
+
+    item.evmActiveUsers.map(x => {
+      if(!evm.includes(x)) {
+        evm.push(x);
+      }  
+    });
+  });
+
+  res.send({
+    nativeUniqueCount: native.length,
+    evmUniqueCount: evm.length
+  });
 });
 
 app.get('/blockdata/monthly', async (req, res) => {
@@ -35,6 +56,12 @@ app.get('/blockdata/monthly', async (req, res) => {
   return res.send(csv);
 });
 
+const getDailyData = async () => {
+  const result = await axios.post(API_URL, {
+    query: 'query { dailyCounts (last:30, orderBy:ID_ASC) { nodes { id, nativeActiveUsers, evmActiveUsers} } }'
+  });
+  return result.data.data.dailyCounts.nodes;
+}
 
 app.listen(port, () => {
   console.log(`The server is  listening on port ${port}`)

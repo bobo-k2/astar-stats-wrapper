15028773d5328e3ebf544ca67c1e577b03a79f3f
Bobo <bobo.kovacevic@gmail.com>
2022-05-09 22:53:05 +0200

    monthly

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const API_URL = 'https://api.subquery.network/sq/AstarStats/astarstats-aggregate
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/blockdata/30days', async (req, res) => {
+app.get('/blockdata/daily/30days', async (req, res) => {
   const result = await axios.post(API_URL, {
     query: 'query { dailyCounts (last:30, orderBy:ID_ASC) { nodes { id, nativeActiveUsers, evmActiveUsers} } }'
   });
@@ -16,9 +16,24 @@ app.get('/blockdata/30days', async (req, res) => {
   });
 
   res.header('Content-Type', 'text/csv');
-  res.attachment('stats.csv');
+  res.attachment('daily.csv');
   return res.send(csv);
-})
+});
+
+app.get('/blockdata/monthly/30days', async (req, res) => {
+  const result = await axios.post(API_URL, {
+    query: 'query { monthlyCounts (last:30, orderBy:ID_ASC) { nodes { id, nativeActiveUsers, evmActiveUsers} } }'
+  });
+  const data = result.data.data.monthlyCounts.nodes;
+  let csv = 'Date, NativeActiveUsersCount, EVMActiveUsersCount\n';
+  data.map(item => {
+    csv += `${item.id}, ${item.nativeActiveUsers.length}, ${item.evmActiveUsers.length}` + '\n';
+  });
+
+  res.header('Content-Type', 'text/csv');
+  res.attachment('monthly.csv');
+  return res.send(csv);
+});
 
 
 app.listen(port, () => {
